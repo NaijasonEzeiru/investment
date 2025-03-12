e1b2cb3bdb66700a98f3a5dcd40adb37df7aff33
NaijasonEzeiru <jayseehe1035@gmail.com>
2025-03-12 16:41:50 +0100

    .................

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -5,7 +5,7 @@ export const apiAddress =
 
 export const VIPTASKS = [
   { tasks: 30, cost: 28.3, reward: 41 },
-  { tasks: 28, cost: 97.5, reward: 150 },
-  { tasks: 25, cost: 368, reward: 500 },
-  { tasks: 15, cost: 1500, reward: 2300 },
+  { tasks: 28, cost: 53.5, reward: 87 },
+  { tasks: 25, cost: 200, reward: 420 },
+  { tasks: 15, cost: 670, reward: 1100 },
 ] as const;
