faad24dd3bde917e18cba1af7cb6ae3ec34f58f7
NaijasonEzeiru <jayseehe1035@gmail.com>
2025-03-12 16:42:17 +0100

    ..................

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -3,6 +3,13 @@ export const apiAddress =
     ? "http://localhost:3000"
     : "https://petsmatchup.vercel.app";
 
+// export const VIPTASKS = [
+//   { tasks: 30, cost: 28.3, reward: 41 },
+//   { tasks: 28, cost: 97.5, reward: 150 },
+//   { tasks: 25, cost: 368, reward: 500 },
+//   { tasks: 15, cost: 1500, reward: 2300 },
+// ] as const;
+
 export const VIPTASKS = [
   { tasks: 30, cost: 28.3, reward: 41 },
   { tasks: 28, cost: 53.5, reward: 87 },
